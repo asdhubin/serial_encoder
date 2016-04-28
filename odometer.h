5ea4083920d6b0434a3595e29640492a42435924
Aria <892845572@qq.com>
2016-04-28 20:40:59 +0800

    第一次上传源代码，还没有完成校验和，代码注释按Doxygen格式书写。

---
+++ odometer.h
@@ -0,0 +1,92 @@
+#ifndef ODOMETER_H_INCLUDED
+#define ODOMETER_H_INCLUDED
+/**
+*class odometer
+*km,m,mm
+*/
+#include <iostream>
+class odometer{
+private:
+    int positive_kilometer=0;
+    int positive_meter=0;
+    int positive_milimeter=0;
+    int negative_kilometer=0;
+    int negative_meter=0;
+    int negative_milimeter=0;
+public:
+    /*void odo_add_mm(int mm);
+    void odo_add_m(int m);
+    void odo_add_km(int km);
+    void odo_print();
+    void odo_print_positive();
+    void odo_print_negative();*/
+
+/**
+*void odo_add_mm()
+*add XX milimeters to odometer
+*/
+void odo_add_mm(int mm){
+    if(mm>=0){
+        positive_milimeter+=mm;
+        positive_meter    +=positive_milimeter/1000;
+        positive_kilometer =positive_meter/1000;
+        positive_meter     =positive_meter%1000;
+        positive_milimeter =positive_milimeter%1000;
+    }
+    else if(mm<0){
+        negative_milimeter+=mm;
+        negative_meter    +=negative_milimeter/1000;
+        negative_kilometer =negative_meter/1000;
+        negative_meter     =negative_meter%1000;
+        negative_milimeter =negative_milimeter%1000;
+    }
+}
+/**
+*void odo_add_m()
+*add XX meters to odometer
+*/
+void odo_add_m(float m){
+    int a=m;//È¡Õû
+    if(m>=0){
+        positive_milimeter+=(m-a)*1000;
+        positive_meter     = positive_meter + a +(positive_milimeter/1000);
+        positive_kilometer = positive_meter/1000;
+        positive_meter     = positive_meter%1000;
+        positive_milimeter = positive_milimeter%1000;
+    }
+    else if(m<0){
+        negative_milimeter+=(m-a)*1000;
+        negative_meter     = negative_meter + a +(negative_milimeter/1000);
+        negative_kilometer = negative_meter/1000;
+        negative_meter     = negative_meter%1000;
+        negative_milimeter = negative_milimeter%1000;
+    }
+}
+/**
+*void odo_print()
+*print both forward distance and backward distance.
+*/
+void odo_print(){
+    std::cout<<"move forward "<<positive_kilometer<<"km "<<positive_meter<<"m "
+    <<positive_milimeter<<"mm"<<std::endl;
+    std::cout<<"move back    "<<negative_kilometer<<"km "<<negative_meter<<"m "
+    <<negative_milimeter<<"mm"<<std::endl;
+}
+/**
+*void odo_print_positive()
+*only print forward distance.
+*/
+void odo_print_positive(){
+    std::cout<<"move forward "<<positive_kilometer<<"km "<<positive_meter<<"m "
+    <<positive_milimeter<<"mm"<<std::endl;
+}
+/**
+*void odo_print_negative()
+*only print backward distance.
+*/
+void odo_print_negative(){
+    std::cout<<"move back    "<<negative_kilometer<<"km "<<negative_meter<<"m "
+    <<negative_milimeter<<"mm"<<std::endl;
+}
+};
+#endif // ODOMETER_H_INCLUDED
---
+++ serial_pro.cpp
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <math.h>
+#include "odometer.h"
+
+#define portname  "/dev/ttyUSB0"
+#define revolution 1000 //every 1000 of encoder number is 1mm
+
+static struct termios oldtty;
+
+int set_interface_attribs (int fd, int speed, int parity)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+
+        if (tcgetattr (fd, &oldtty) != 0)
+        {
+                // error_message ("error %d from tcgetattr", errno);
+                printf("error opening the device");
+                return -1;
+        }
+
+        memcpy( &tty,  &oldtty, sizeof (struct termios) );
+
+        cfsetospeed (&tty, speed);
+        cfsetispeed (&tty, speed);
+
+        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+        // disable IGNBRK for mismatched speed tests; otherwise receive break
+        // as \000 chars
+        tty.c_iflag &= ~IGNBRK;         // disable break processing
+        tty.c_lflag = 0;                // no signaling chars, no echo,
+                                    // no canonical processing
+        tty.c_oflag = 0;                // no remapping, no delays
+        tty.c_cc[VMIN]  = 0;            // read doesn't block
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+                                    // enable reading
+        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+        tty.c_cflag |= parity;
+        tty.c_cflag &= ~CSTOPB;
+        tty.c_cflag &= ~CRTSCTS;
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+        {
+            // error_message ("error %d from tcsetattr", errno);
+            printf("error opening the device");
+            return -1;
+        }
+
+        return 0;
+} // end function: set_interface_attribs
+
+
+int set_blocking (int fd, int should_block)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        if (tcgetattr (fd, &tty) != 0)
+        {
+            //error_message ("error %d from tggetattr", errno);
+            printf("error opening the device");
+            return -1;
+        }
+
+        tty.c_cc[VMIN]  = should_block ? 1 : 0;
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+          //  error_message ("error %d setting term attributes", errno);
+            printf("error opening the device");
+    return 0;
+} // end function: set_blocking
+
+
+int main()
+{
+    int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
+
+    if (fd < 0)
+    {
+        printf("error opening the device\n");
+    }
+
+
+    /*CHANGES*/
+    if(!0)//set_interface_attribs(fd, B9600, 0)!=0)
+    {            printf("hello");
+        printf("error set interface\n");
+    }
+
+    else
+
+
+    if(set_blocking(fd, 0)!=0)
+    {
+        printf("error set blocking\n");
+    }
+
+    else
+        printf("done");
+
+    // what is this stray end of comment?
+    // suggest enabling all the compiler warings
+    // and fixing the warnings
+    // */
+    if( set_interface_attribs (fd, B9600, 0) )
+    { // then set_interface_attribs failed
+        return -1;
+    }
+
+    // implied else set_interface_attribs successful
+
+    if( set_blocking (fd, 0) )                // set no blocking
+    { // then set_blocking failed
+        return -1;   // might need to also restore oldtty attributes
+    }
+
+    // implied else, set_blocking successful
+
+
+    char receivebuffer [20];
+
+
+
+    // implied else, read successful
+    //initialize parameter
+    unsigned char A5A5[2];A5A5[0]=0xA5;A5A5[1]=0xA5;
+
+    int old_data=0;int new_data=0;
+    int delta;
+    class odometer test1;
+    unsigned char var[4];//for saving 4bytes raw data
+
+    while(1){
+        write (fd,A5A5 , 2);
+
+        usleep (3000);
+    if( 6> read (fd, receivebuffer, sizeof receivebuffer) )
+        { // then read failed
+        return -1;
+        break;
+    }
+
+        /*printf("value of buffer is %2d %2d %2d %2d %2d %2d \n\n",
+        receivebuffer[0],
+        receivebuffer[1],
+        receivebuffer[2],
+        receivebuffer[3],
+        receivebuffer[4],
+        receivebuffer[5]
+        );*/  //here is test output,output like E1 9B FE FF FF 79
+    for(int i=0;i<4;i++){
+	    var[i]=receivebuffer[i+1];
+    }
+    old_data=new_data;
+	new_data=(var[3]<<24)|(var[2]<<16)|(var[1]<<8)|(var[0]);
+    delta=new_data-old_data;
+
+	test1.odo_add_mm(delta/revolution);
+	test1.odo_print();
+
+
+    }
+
+
+    // cleanup
+    tcsetattr (fd, TCSANOW, &oldtty);
+    return 0;
+} // end function: main
